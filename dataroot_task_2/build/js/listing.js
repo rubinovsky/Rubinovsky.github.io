270cee11182cc3705fdfb557dc63729fbe2dce57
Oles Rubinovsky <oles.rubinovscky@gmail.com>
2017-04-29 00:16:39 +0300

    fixed bug with require window

--- dataroot_task_2/build/js/listing.js
+++ dataroot_task_2/build/js/listing.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
 		$('.js-blur').toggleClass('wrap-blur')
 		$(this).toggleClass('hamburger__icon-active')
 	})
-	$('.js-slider').listingSlider();
+	// Дефолтні настройки слайдера, прописав для швидкого доступу
+	$('.js-slider').listingSlider({infinite: true, index: 0, arrows: true, swipeAble: true, autoplay: false, pagination: true, autoplaySpeed: 5});
+	
 	$('.footer__button').modal();
 	$('.js-request-slct').select({value:'holoseevskyy', autocomplete: true, placeholder:'Выберите район'});
 });
