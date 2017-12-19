71607ff94ad120b553f108e481792c8e108e3b6a
Oles Rubinovsky <oles.rubinovscky@gmail.com>
2017-12-20 01:01:12 +0200

    Sticky Header(script)

--- sticky_header/script.js
+++ sticky_header/script.js
@@ -1,22 +1,20 @@
-// ******** ПОЧАТОК Sticky Header для UC BROWSER
-if (navigator.userAgent.match(/^Mozilla\/5\.0 .+ Gecko\/$/)) {
-	$(document).ready(function(){
-		var header = $('header');
-		var offesHeader = header.offset().top;
-		var windowScroll = $(window);
-	    header.after("<div class='pseudo_header pseudo_header-hidden'></div>");
-	    var pseudo_header = $('.pseudo_header');
-	    pseudo_header.css('height', header.outerHeight());
-		windowScroll.scroll(function() {
-			if (windowScroll.scrollTop() >= offesHeader && !header.hasClass('sticky')) {
-				header.addClass('sticky');
-				pseudo_header.removeClass('pseudo_header-hidden');
-			}
-			else if(windowScroll.scrollTop() < offesHeader){
-				header.removeClass('sticky');
-				pseudo_header.addClass('pseudo_header-hidden');
-			}
-		})
-	});
-}
-// ******** КІНЕЦЬ Sticky Header для UC BROWSER
+// ******** ПОЧАТОК Sticky Header 
+$(document).ready(function(){
+	var header = $('header');
+	var offesHeader = header.offset().top;
+	var windowScroll = $(window);
+    header.after("<div class='pseudo_header pseudo_header-hidden'></div>");
+    var pseudo_header = $('.pseudo_header');
+    pseudo_header.css('height', header.outerHeight());
+	windowScroll.scroll(function() {
+		if (windowScroll.scrollTop() >= offesHeader && !header.hasClass('sticky')) {
+			header.addClass('sticky');
+			pseudo_header.removeClass('pseudo_header-hidden');
+		}
+		else if(windowScroll.scrollTop() < offesHeader){
+			header.removeClass('sticky');
+			pseudo_header.addClass('pseudo_header-hidden');
+		}
+	})
+});
+// ******** КІНЕЦЬ Sticky Header
