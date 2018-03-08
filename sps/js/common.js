cf1e1e531516947a8e2cda5ab2484f2e0b282f98
Oles Rubinovsky <oles.rubinovscky@gmail.com>
2018-03-09 01:03:37 +0200

    SPS v.2

--- sps/js/common.js
+++ sps/js/common.js
@@ -8,30 +8,14 @@ $(document).ready(function(){
 	}
 	window.dirItemHover = false;
 	window.changeSlide = false;
+	$('#about_us').scrollTop(0);
 	$('nav a').click(function(e){
 		e.preventDefault();
 		$('nav a').removeClass('active');
 		$(this).addClass('active');
 		var sectionId = $(this).attr('href');
 		var offsetSection = $(sectionId).data('scrollTo');
-		var sectionBG = $(sectionId).data('bg');
 		$('body').animate({'top': '-' + offsetSection + 'px' }, 500);
-		switch (sectionBG) {
-		case 'yellow':
-			$('body').removeClass('yellow gray blue');
-			$('body').addClass('yellow');
-			break;
-		case 'gray':
-			$('body').removeClass('yellow gray blue');
-			$('body').addClass('gray');
-			break;
-		case 'blue':
-			$('body').removeClass('yellow gray blue');
-			$('body').addClass('blue');
-			break;
-		default:
-			$('body').removeClass('yellow gray blue');
-		}
 	});
 	$('body').on('mousewheel', function(event) {
 		if (window.changeSlide == false) {
@@ -61,24 +45,4 @@ $(document).ready(function(){
 			}
 		}
 	});
-	$('.direction__item').hover(function(){
-		window.dirItemHover = true;
-			$('.direction__item').removeClass('hover');
-			$(this).addClass('hover');
-	}, function(){
-		window.dirItemHover = false;
-			$(this).removeClass('hover');
-	})
-	function blink(){
-		if (!window.dirItemHover) {
-			var dir = $('.direction__item');
-			dir.removeClass('hover');
-			dir.eq(getRandomInt(0, dir.length)).addClass('hover');		
-		}
-	}
-
-    function getRandomInt(min, max){
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    var blinktimer = setInterval(blink, 3000);
 })
