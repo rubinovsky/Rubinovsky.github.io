a080d5243d8f03da04c9c5f6f1a45666f093cc41
Macbook <oles.rubinovskiy@gmail.com>
2019-01-28 05:39:58 +0200

    feat: Timer Calculator

---
+++ timer_calc/precache-manifest.6e6f5fa8d9c66f48c7dfd3dc9a94474d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00d569333cbea9a2bb8d",
+    "url": "/static/css/main.ff4ad882.chunk.css"
+  },
+  {
+    "revision": "00d569333cbea9a2bb8d",
+    "url": "/static/js/main.00d56933.chunk.js"
+  },
+  {
+    "revision": "a67624bdcbb8aa564f80",
+    "url": "/static/js/1.a67624bd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08f9ef09a9d520b90efafb59426181d7",
+    "url": "/index.html"
+  }
+];
